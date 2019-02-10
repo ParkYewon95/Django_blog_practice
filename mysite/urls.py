fb7b30199156335f9c4252913310ee016aba38b8
yewon <angelwon53@gmail.com>
2019-02-10 18:02:55 +0900

    static파일 수정

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,8 +21,8 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    #url(r'^admin/login/$', views.login, name='login'),
-    #url(r'^accounts/login/$', views.login, name='login'),
+#    url(r'^admin/login/$', views.login, name='login'),
+#    url(r'^accounts/login/$', views.login, name='login'),
 
     #url(r'^admin/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 
