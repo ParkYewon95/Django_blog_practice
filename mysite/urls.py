47a6cff1877c7bf2de728ca34cbb1a693112c434
yewon <angelwon53@gmail.com>
2019-02-20 15:55:57 +0900

    Changed html, url root

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url , include
 from django.contrib import admin
 
 from django.contrib.auth import views
@@ -26,5 +26,5 @@ urlpatterns = [
 
  #  url(r'^admin/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 
-#url(r'', include('blog.urls')),
+    url(r'', include('blog.urls')),
 ]
